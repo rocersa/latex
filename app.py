fc92a9c49e34d60690a384f359cb2cad4c4b5f83
harrybirdwork <hwb26uc@gmail.com>
2024-08-15 12:44:37 +1200

    changed client latex

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ def generate_pdf():
     invoice = data.get('invoice')
     totalPrice = data.get('totalPrice')
     latex = data.get('latex')
-    print(latex)
-    print(type(latex))
+    
     if not invoice or not latex:
         return jsonify({"error": "Invalid input"}), 400
     
@@ -22,7 +21,7 @@ def generate_pdf():
     
     # Write LaTeX source to a file
     with open('invoice.tex', 'w') as f:
-        f.write(latex_source)
+        f.write(latex)
     
     # Run pdflatex to generate PDF
     subprocess.run(['pdflatex', 'invoice.tex'], check=True)
