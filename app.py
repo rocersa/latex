d9bd1a327f4fab6b38005494b1fbc63b83009932
harrybirdwork <hwb26uc@gmail.com>
2024-09-24 11:51:22 +1200

    New CORS

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 import datetime
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/generate-pdf-invoice', methods=['POST'])
 def generate_pdf_invoice():
