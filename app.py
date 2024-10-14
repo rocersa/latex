6aae4f13a516d0c92a81ff2f32d3d074d7b9bac9
harrybirdwork <hwb26uc@gmail.com>
2024-10-15 12:02:31 +1300

    Added US Picklist

--- app.py
+++ app.py
@@ -234,6 +234,8 @@ def generate_pdf_picklist():
     # Generate LaTeX source code
     if country == 'UK':
         latex_source = generate_latex_picklist_uk(invoice, info, components)
+    elif country == 'US':
+        latex_source = generate_latex_picklist_us(invoice, info, components)
     
     # Use a secure temporary directory
     with tempfile.TemporaryDirectory() as tmpdirname:
@@ -357,6 +359,101 @@ def picklist_table_rows_uk(invoice, components):
     table_rows = ""
     for product in components:
         table_rows += f"\\texttt{{{product['ProductsT']['ProductCode']}}} & \\texttt{{{product['ProductsT']['NameMetric']}}} & \\texttt{{{product['Quantity']}}} & \\texttt{{{(product['ProductsT']['Weight'] * product['Quantity']):.1f}}}  \\\\ \n"
+        table_rows += "\\hline \n"
+    return table_rows
+
+def generate_latex_picklist_us(invoice, info, components):
+    # Generate LaTeX content here (similar to the LaTeX source in your Node.js example)
+    latex_source = f"""
+    \\documentclass[a4paper,12pt]{{article}}
+    \\usepackage{{graphicx}}
+    \\usepackage{{geometry}}
+    \\geometry{{a4paper, margin=1cm}}
+    \\usepackage{{array}}
+    \\usepackage{{longtable}}
+    \\usepackage{{anyfontsize}}
+    \\pagestyle{{empty}}
+
+    \\begin{{document}}
+    
+    \\vspace{{0.5cm}}
+
+    \\noindent
+    \\texttt{{Deliver to:}} \\\\
+
+    \\textbf{{\\fontsize{{20}}{{24}}\\selectfont {invoice['customers']['first_name']}}} \\textbf{{\\fontsize{{20}}{{24}}\\selectfont {invoice['customers']['last_name']}}} \\\\
+    """
+    latex_source += f"""
+    \\textbf{{\\fontsize{{30}}{{36}}\\selectfont {invoice['addresses']['street_address']}}} \\\\
+ 
+    \\textbf{{\\fontsize{{50}}{{60}}\\selectfont {invoice['addresses']['suburb']}}} \\\\
+
+    \\textbf{{\\fontsize{{70}}{{84}}\\selectfont {invoice['addresses']['postal_code']}}} \\\\
+
+    \\noindent
+    \\textsf{{\\Large {{Email: }}}} \\textsf{{\\Large {invoice['customers']['email']}}} \\\\
+    \\textsf{{\\Large {{Phone: }}}} \\textsf{{\\Large {invoice['customers']['phone']}}} \\\\
+
+    \\vspace{{0.5cm}}
+    
+    \\noindent
+    \\begin{{minipage}}[t]{{0.45\\textwidth}}
+        \\raggedright
+        \\small
+        COR-TEN-STEEL USA \\\\
+        Mesa Street \\\\
+        Hesperia \\\\
+        92345 CA \\\\
+        Tel: 760-995-2555 \\\\
+        Email: usa@cor-ten-steel.com \\\\
+ 
+    \\end{{minipage}}
+    \\hfill
+    \\begin{{minipage}}[t]{{0.45\\textwidth}}
+        \\raggedleft
+        \\small
+        Picklist \\\\
+        \\vspace{{1cm}}
+        Invoice Number: \\texttt{{{str(invoice['InvoiceID']).zfill(5)}}} \\\\
+        Date Issued: \\texttt{{{us_time.strftime("%d-%b-%Y %H:%M")}}} 
+    \\end{{minipage}}
+    
+    \\vspace{{0.5cm}}
+
+    \\noindent
+    \\rule{{\\textwidth}}{{0.5pt}}
+
+    \\vspace{{0.5cm}}
+
+    \\noindent
+    \\begin{{tabular}}{{l l}}
+    \\textbf{{Total Items:}} & \\texttt{{{info['total_items']}}} \\\\ 
+    \\textbf{{Total Weight:}} & \\texttt{{{info['total_weight']}}} \\\\ 
+    \\textbf{{Carrier:}} & \\texttt{{{invoice['freight_carrier']}}} \\\\ 
+    \\textbf{{Stickers:}} & \\texttt{{{invoice['stickers']}}} \\\\ 
+    \\textbf{{Packing Instructions:}} & \\texttt{{{invoice['packing_instructions']}}} \\\\ 
+    \\textbf{{Con Note:}} & \\texttt{{{invoice['con_note']}}} \\\\ 
+    \\end{{tabular}}
+
+    \\vspace{{0.5cm}}
+
+    \\begin{{longtable}}{{|l|l|l|l|}}
+        \\hline
+        \\textbf{{Code}} & \\textbf{{Description}} & \\textbf{{Qty}} & \\textbf{{Weight (kgs)}} \\\\
+        \\hline
+        {picklist_table_rows_us(invoice, components)}
+    \\end{{longtable}}
+    
+
+
+    \\end{{document}}
+    """
+    return latex_source
+
+def picklist_table_rows_us(invoice, components):
+    table_rows = ""
+    for product in components:
+        table_rows += f"\\texttt{{{product['ProductsT']['ProductCode']}}} & \\texttt{{{product['ProductsT']['NameImperial']}}} & \\texttt{{{product['Quantity']}}} & \\texttt{{{(product['ProductsT']['Weight'] * product['Quantity']):.1f}}}  \\\\ \n"
         table_rows += "\\hline \n"
     return table_rows
 
