2623dc3d72e60eef2e010ee51b5bbc93fb71a8d0
harrybirdwork <hwb26uc@gmail.com>
2024-10-17 17:16:32 +1300

    updated to use editable component price

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def generate_latex_invoice_uk(invoice):
 def invoice_table_rows_uk(invoice):
     table_rows = ""
     for product in invoice["InvoiceComponentsT"]:
-        table_rows += f"\\texttt{{{product['ProductsT']['NameMetric']}}} & \\texttt{{{product['Quantity']}}} & \\texttt{{£{product['ProductsT']['UKRetailCost']:.2f}}} & \\texttt{{£{(product['ProductsT']['UKRetailCost'] * product['Quantity']):.2f}}} \\\\ \n"
+        table_rows += f"\\texttt{{{product['ProductsT']['NameMetric']}}} & \\texttt{{{product['Quantity']}}} & \\texttt{{£{product['price']:.2f}}} & \\texttt{{£{(product['price'] * product['Quantity']):.2f}}} \\\\ \n"
         table_rows += "\\hline \n"
     return table_rows
 
@@ -219,7 +219,7 @@ def generate_latex_invoice_us(invoice):
 def invoice_table_rows_us(invoice):
     table_rows = ""
     for product in invoice["InvoiceComponentsT"]:
-        table_rows += f"\\texttt{{{product['ProductsT']['NameImperial']}}} & \\texttt{{{product['Quantity']}}} & \\texttt{{\\${product['ProductsT']['USRetailCost']:.2f}}} & \\texttt{{\\${(product['ProductsT']['USRetailCost'] * product['Quantity']):.2f}}} \\\\ \n"
+        table_rows += f"\\texttt{{{product['ProductsT']['NameImperial']}}} & \\texttt{{{product['Quantity']}}} & \\texttt{{\\${product['price']:.2f}}} & \\texttt{{\\${(product['price'] * product['Quantity']):.2f}}} \\\\ \n"
         table_rows += "\\hline \n"
     return table_rows
 
