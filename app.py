61bd9c9a8c208e1212d2d6d0332019025dca9559
harrybirdwork <hwb26uc@gmail.com>
2024-11-28 10:37:05 +1300

    yay!

--- app.py
+++ app.py
@@ -119,24 +119,24 @@ def generate_latex_invoice_uk(invoice, uk_time):
         \\hline
         {invoice_table_rows_uk(invoice)}
         \\hline
-        \\multicolumn{{2}}{{c|}}{{}} & Subtotal & £\\texttt{{{((invoice['Price'] - invoice['freight_charged']) * (5/6)):.2f}}} \\\\
+        \\multicolumn{{2}}{{c|}}{{}} & Subtotal & \\multicolumn{{1}}{{r|}}\\texttt{{£{((invoice['Price'] - invoice['freight_charged']) * (5/6)):.2f}}} \\\\
         \\cline{{3-4}}
-        \\multicolumn{{2}}{{c|}}{{}} & Freight & £\\texttt{{{(invoice['freight_charged'] * (5/6)):.2f}}} \\\\
+        \\multicolumn{{2}}{{c|}}{{}} & Freight & \\multicolumn{{1}}{{r|}}\\texttt{{£{(invoice['freight_charged'] * (5/6)):.2f}}} \\\\
         \\cline{{3-4}}
-        \\multicolumn{{2}}{{c|}}{{}} & VAT & £\\texttt{{{(invoice['Price']*(1/6)):.2f}}} \\\\
+        \\multicolumn{{2}}{{c|}}{{}} & VAT & \\multicolumn{{1}}{{r|}}\\texttt{{£{(invoice['Price']*(1/6)):.2f}}} \\\\
         \\cline{{3-4}}"""
     if invoice['amount_paid']:
         latex_source += f"""
-        \\multicolumn{{2}}{{c|}}{{}} & Total inc VAT & £\\texttt{{{invoice['Price']:.2f}}} \\\\
+        \\multicolumn{{2}}{{c|}}{{}} & Total inc VAT & \\multicolumn{{1}}{{r|}}\\texttt{{£{invoice['Price']:.2f}}} \\\\
         \\cline{{3-4}}
-        \\multicolumn{{2}}{{c|}}{{}} & Amount Paid & £\\texttt{{{invoice['amount_paid']:.2f}}} \\\\
+        \\multicolumn{{2}}{{c|}}{{}} & Amount Paid & \\multicolumn{{1}}{{r|}}\\texttt{{£{invoice['amount_paid']:.2f}}} \\\\
         \\cline{{3-4}}
-        \\multicolumn{{2}}{{c|}}{{}} & Balance due inc VAT & £\\texttt{{{(invoice['Price'] - invoice['amount_paid']):.2f}}} \\\\
+        \\multicolumn{{2}}{{c|}}{{}} & Balance due inc VAT & \\multicolumn{{1}}{{r|}}\\texttt{{£{(invoice['Price'] - invoice['amount_paid']):.2f}}} \\\\
         \\cline{{3-4}}
         """
     else:
         latex_source += f"""
-        \\multicolumn{{2}}{{c|}}{{}} & Balance due inc VAT & £\\texttt{{{(invoice['Price']):.2f}}} \\\\
+        \\multicolumn{{2}}{{c|}}{{}} & Balance due inc VAT & \\multicolumn{{1}}{{r|}}\\texttt{{{(invoice['Price']):.2f}}} \\\\
         \\cline{{3-4}}"""
     latex_source += f"""
     \\end{{longtable}}
@@ -156,7 +156,7 @@ def generate_latex_invoice_uk(invoice, uk_time):
 def invoice_table_rows_uk(invoice):
     table_rows = ""
     for product in invoice["InvoiceComponentsT"]:
-        table_rows += f"\\texttt{{{product['ProductsT']['NameMetric']}}} & \\texttt{{{product['Quantity']}}} & \\multicolumn{{1}}{{r}}{{\\texttt{{£{(product['price'] * (5/6)):.2f}}}}} & \\multicolumn{{1}}{{r}}{{\\texttt{{£{(product['price'] * product['Quantity'] * (5/6)):.2f}}}}} \\\\ \n"
+        table_rows += f"\\texttt{{{product['ProductsT']['NameMetric']}}} & \\texttt{{{product['Quantity']}}} & \\multicolumn{{1}}{{r|}}{{\\texttt{{£{(product['price'] * (5/6)):.2f}}}}} & \\multicolumn{{1}}{{r|}}{{\\texttt{{£{(product['price'] * product['Quantity'] * (5/6)):.2f}}}}} \\\\ \n"
         table_rows += "\\hline \n"
     return table_rows
 
