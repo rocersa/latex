1650d0f21404242928cfb3f29654fb03fbd9230c
harry <harry@rocersa.nz>
2025-04-20 13:32:49 +1200

    bracewire logic update

--- app.py
+++ app.py
@@ -434,8 +434,7 @@ def picklist_table_rows(components, country):
         table_rows += f"\\texttt{{{escape_latex(product['products']['code'])}}} & \\texttt{{{escape_latex(product['products'][name_key])}}} & \\texttt{{{product['quantity']}}} & \\texttt{{{(product['products']['weight'] * product['quantity'] * weight_multiplier):.1f}}} \\\\ \n"
         table_rows += "\\hline \n"
     total_bracewire = sum(
-    (component['products']['bracewire'] or 0) * component['quantity']
-    for component in components if 'bracewire' in component['products']
+    (component['products']['bracewire'] or 0) * component['quantity'] for component in components
     )   
     if total_bracewire > 0:
         table_rows += f"\\texttt{{GabBra}} & \\texttt{{Bracewire}} & \\texttt{{1}} & \\texttt{{{total_bracewire:.1f}}} \\\\ \n"
